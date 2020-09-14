dc9b49f1bcf072333852db400f1f52d3f3415dd5
Ivan Semenov <ivan@semenov.dev>
2020-09-14 18:19:51 +0300

    build script update

--- build.sh
+++ build.sh
@@ -37,15 +37,16 @@ wget http://download.geofabrik.de/europe/cyprus-latest.osm.pbf -O $TMP_DIR/cypru
 java -cp $MKGMAP_DIR/mkgmap.jar uk.me.parabola.mkgmap.main.TypCompiler "$TYP_NAME.txt" "$OUT_DIR/$TYP_NAME.typ"
 
 # Build map
-java -Xmx1G -jar $MKGMAP_DIR/mkgmap.jar --verbose --output-dir=$OUT_DIR \
+java -Xmx1G -jar $MKGMAP_DIR/mkgmap4377.jar --verbose --output-dir=$OUT_DIR \
       --precomp-sea=$MKGMAP_DIR/sea.zip --bounds=$MKGMAP_DIR/bounds.zip \
       --mapname="$MAPNAME" --description="$MAP_DESCR" --family-name="$MAP_FAMILY_NAME" \
       --product-id=1 --family-id="$MAP_FAMILY_ID" --series-name="$MAP_SERIES_NAME" \
       --country-name=Cyprus --country-abbr=CY \
       --style-file=styles --style="$STYLE" \
+      --style-option=code-page=1250 \
       --gmapsupp --drive-on=left --gmapi \
       --name-tag-list=name:en,int_name,name \
-      --code-page=1252 -c optionsfile.args \
+      --latin1 -c optionsfile.args \
       --dem="$SRTM_DIR" \
       --dem-dists=3312,13248,26512,53024 \
       "$OUT_DIR/$TYP_NAME.typ" \
@@ -63,14 +64,14 @@ phyghtmap --step=10 --line-cat=400,50 --pbf \
       --srtm-version=3.0 "$SRTM_DIR"/*
 cd -
 
-java -Xmx1G -jar $MKGMAP_DIR/mkgmap.jar --verbose --output-dir=$CONTOUR_DIR \
+java -Xmx1G -jar $MKGMAP_DIR/mkgmap4377.jar --verbose --output-dir=$CONTOUR_DIR \
       --country-name=Cyprus --country-abbr=CY \
       --max-jobs=3 --gmapsupp \
       --mapname=$CONTOUR_MAPNAME \
-      --bounds=$MKGMAP_DIR/bounds.zip \
-      --merge-lines --draw-priority=28 \
+      --transparent --merge-lines --draw-priority=28 \
       --description="cyprus-contours" \
       --style-file=styles --style="$STYLE" \
+      --style-option=code-page=1250 \
       "$OUT_DIR/$TYP_NAME.typ" $CONTOUR_DIR/contour_*.pbf 
 
 cp $CONTOUR_DIR/gmapsupp.img $MAP_DIR/gmapsupp-contours.img
