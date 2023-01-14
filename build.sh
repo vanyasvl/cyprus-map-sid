dc2f0370bd108f1344903dc4dd7cf8839853e114
Ivan Semenov <sid@servers.com>
2023-01-14 19:40:43 +0200

    Weekly builds with GitHub Actions

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-MKGMAP_DIR="/Users/sid/maps/mkgmap"
 MAPNAME=41339000
 MAP_FAMILY_ID=41339
 MAP_DESCR="Cyprus OSM sid"
@@ -10,50 +9,43 @@ MAP_SERIES_NAME="Cyprus OSM sid"
 STYLE="maptourist-sid"
 TYP_FILE="OSM-2018.txt"
 
-TMP_DIR="./tmp"
-MAP_DIR="./map"
-OUT_DIR="./output"
+MKGMAP_DIR="${MKGMAP_DIR:-/tmp/mkgmap}"
 
-SRTM_URL="http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11"
-CYPRUS_MAP_URL="http://download.geofabrik.de/europe/cyprus-latest.osm.pbf"
+SRTM_URL="https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11"
+CYPRUS_MAP_URL="https://download.geofabrik.de/europe/cyprus-latest.osm.pbf"
 
-mkdir -p "$TMP_DIR" "$OUT_DIR" "$MAP_DIR"
-
-echo Cleaning old output files
-rm -rf "$OUT_DIR"/*
-echo Cleaning old temp files
-rm -rf "$TMP_DIR"/*
+mkdir -p tmp output srtm_downloads
 
 echo Downloading Cyprus map
-wget -q --show-progress "$CYPRUS_MAP_URL" -O "$TMP_DIR/cyprus.osm.pbf"
+wget -nv -q "$CYPRUS_MAP_URL" -O "tmp/cyprus.osm.pbf"
 
 echo Downloading srtm files
-wget -q --show-progress --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N34E032.SRTMGL1.hgt.zip" -O "$TMP_DIR/N34E032.SRTMGL1.hgt.zip"
-wget -q --show-progress --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N34E033.SRTMGL1.hgt.zip" -O "$TMP_DIR/N34E033.SRTMGL1.hgt.zip"
-wget -q --show-progress --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N34E034.SRTMGL1.hgt.zip" -O "$TMP_DIR/N34E034.SRTMGL1.hgt.zip"
-wget -q --show-progress --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N35E032.SRTMGL1.hgt.zip" -O "$TMP_DIR/N35E032.SRTMGL1.hgt.zip"
-wget -q --show-progress --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N35E033.SRTMGL1.hgt.zip" -O "$TMP_DIR/N35E033.SRTMGL1.hgt.zip"
-wget -q --show-progress --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N35E034.SRTMGL1.hgt.zip" -O "$TMP_DIR/N35E034.SRTMGL1.hgt.zip"
+wget -nv -q -c --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N34E032.SRTMGL1.hgt.zip" -O "srtm_downloads/N34E032.SRTMGL1.hgt.zip"
+wget -nv -q -c --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N34E033.SRTMGL1.hgt.zip" -O "srtm_downloads/N34E033.SRTMGL1.hgt.zip"
+wget -nv -q -c --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N34E034.SRTMGL1.hgt.zip" -O "srtm_downloads/N34E034.SRTMGL1.hgt.zip"
+wget -nv -q -c --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N35E032.SRTMGL1.hgt.zip" -O "srtm_downloads/N35E032.SRTMGL1.hgt.zip"
+wget -nv -q -c --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N35E033.SRTMGL1.hgt.zip" -O "srtm_downloads/N35E033.SRTMGL1.hgt.zip"
+wget -nv -q -c --user "$USGS_USER" --password "$USGS_PASSWORD" "$SRTM_URL/N35E034.SRTMGL1.hgt.zip" -O "srtm_downloads/N35E034.SRTMGL1.hgt.zip"
 
 echo Unpacking srtm files
-unzip -q -o "$TMP_DIR"/\*.zip -d "$TMP_DIR"
+unzip -q -o "srtm_downloads/*.zip" -d "./tmp"
 
 echo Building contour lines
-cd "$TMP_DIR"
+cd ./tmp
 phyghtmap --step=10 --line-cat=400,50 --pbf \
-      --output-prefix=contour --source=srtm1 \
+      --output-prefix=contour \
       --no-zero-contour \
-      --srtm-version=3.0 *.hgt
+      --srtm-version=3.0 ./*.hgt
 cd -
 
 echo Merging contour maps
-osmium merge "$TMP_DIR"/contour_*.pbf -o "$TMP_DIR/contours.pbf"
+osmium merge ./tmp/contour_*.pbf -o ./tmp/contours.pbf
 echo Cutting contour map
-osmium extract --polygon cyprus.poly "$TMP_DIR/contours.pbf" -o "$TMP_DIR/cyprus_contours.pbf"
+osmium extract --polygon cyprus.poly ./tmp/contours.pbf -o ./tmp/cyprus_contours.pbf
 
 echo Building map
-java -Xmx1G -jar "$MKGMAP_DIR/mkgmap.jar" --verbose --output-dir="$OUT_DIR" \
-      --precomp-sea="$MKGMAP_DIR/sea.zip" --bounds="$MKGMAP_DIR/bounds.zip" \
+java -Xmx3G -jar "${MKGMAP_DIR}/mkgmap.jar" --verbose --max-jobs=2 --output-dir="./output" \
+      --precomp-sea="${MKGMAP_DIR}/sea-latest.zip" --bounds="${MKGMAP_DIR}/bounds-latest.zip" \
       --mapname="$MAPNAME" --description="$MAP_DESCR" --family-name="$MAP_FAMILY_NAME" \
       --product-id=1 --family-id="$MAP_FAMILY_ID" --series-name="$MAP_SERIES_NAME" \
       --country-name=Cyprus --country-abbr=CY \
@@ -62,13 +54,16 @@ java -Xmx1G -jar "$MKGMAP_DIR/mkgmap.jar" --verbose --output-dir="$OUT_DIR" \
       --gmapsupp --drive-on=left --gmapi \
       --name-tag-list=name:en,int_name,name \
       --latin1 -c optionsfile.args \
-      --dem="$TMP_DIR" --dem-poly=cyprus.poly \
+      --dem="./tmp" --dem-poly=cyprus.poly \
       --dem-dists=3312,13248,26512,53024 \
-      "$TMP_DIR/cyprus.osm.pbf" "$TYP_FILE" \
+      "./tmp/cyprus.osm.pbf" "$TYP_FILE" \
       --transparent --merge-lines --draw-priority=28 \
-      --description="Cyprus contours" \
-      "$TMP_DIR/cyprus_contours.pbf"
+      "./tmp/cyprus_contours.pbf"
 
-cp -r "$OUT_DIR/gmapsupp.img" "$OUT_DIR"/*.gmap "$MAP_DIR"
+echo Compressing maps
+cd ./output
+zip -r "Cyprus OSM sid.gmap.zip" "Cyprus OSM sid.gmap"
+zip -r "gmapsupp.img.zip" "gmapsupp.img"
+cd -
 
-echo All done
+echo "Build is finished"
